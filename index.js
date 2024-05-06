56a066bd049515455e6f95413424c3c0d0208167
StasGoryachev <goriachev07@mail.ru>
2024-05-06 17:50:46 +0300

    scroll-js

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-import '@/scss/index.scss'
+import "@/scss/index.scss";
 
-console.log('hello world')
+const header = document.querySelector(".header");
+const body = document.body;
+
+let headerHeight = header.offsetHeight;
+console.log("headerHeight:", headerHeight);
+
+window.addEventListener("resize", (e) => {
+  headerHeight = header.offsetHeight;
+  console.log("headerHeight:", headerHeight);
+});
+
+window.addEventListener("scroll", (e) => {
+  const scrollDistance = window.scrollY;
+  if (scrollDistance > 99) {
+    header.classList.add("header_fixed");
+    body.style.paddingTop = `${headerHeight}px`;
+  } else {
+    header.classList.remove("header_fixed");
+    body.style.paddingTop = 0;
+  }
+});
+
+const adjustElementPosition = (el) => {
+  const rect = el.getBoundingClientRect();
+  console.log("adjustElementPosition ~ rect:", rect);
+};
+
+const choices = document.querySelectorAll(".choices");
+
+choices.forEach((choice) => {
+  console.log("choices.forEach ~ choices:", choices);
+  const btn = choice.querySelector(".choices__btn");
+  const box = choice.querySelector(".choices__box");
+
+  btn.addEventListener("click", () => {
+    box.classList.toggle("choices__box_open");
+    adjustElementPosition(box);
+  });
+});
