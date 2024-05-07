a55566adaa145743a8815673869b1a44ca9d9ad6
StasGoryachev <goriachev07@mail.ru>
2024-05-07 16:37:20 +0300

    styles for cart

--- index.js
+++ index.js
@@ -26,15 +26,37 @@ const adjustElementPosition = (el) => {
   const rect = el.getBoundingClientRect();
   console.log("adjustElementPosition ~ rect:", rect);
   const viewportWidth = window.innerWidth;
-  console.log("adjustElementPosition ~ viewportWidth:", viewportWidth)
-  if(rect.left < 0) {
-   console.log('выпал влево')
+  console.log("adjustElementPosition ~ viewportWidth:", viewportWidth);
+  console.log(rect);
+  if (rect.left < 0) {
+    el.style.left = "0";
+    el.style.right = "auto";
+    el.style.transform = "translateX(0)";
+  } else if (rect.right > viewportWidth) {
+    el.style.left = "auto";
+    el.style.right = "0";
+    el.style.transform = "translateX(0)";
+  } else {
+    el.style.left = "";
+    el.style.right = "";
+    el.style.transform = "";
   }
 
-  if(rect.right > viewportWidth) {
-    console.log('выпал вправо')
+  const postRect = element.getBoundingClientRect();
+
+  if ((postRect.left < 0 || postRect.right > viewportWidth) && count < 3) {
+    count++;
+    adjustElementPosition(element, count);
   }
-  
+};
+
+const clearOpenClasses = () => {
+  document.querySelectorAll(".choices__btn").forEach((btn) => {
+    btn.classList.remove("choices__btn_open");
+  });
+  document.querySelectorAll(".choices__box").forEach((box) => {
+    box.classList.remove("choices__box_open");
+  });
 };
 
 const choices = document.querySelectorAll(".choices");
@@ -44,8 +66,21 @@ choices.forEach((choice) => {
   const btn = choice.querySelector(".choices__btn");
   const box = choice.querySelector(".choices__box");
 
-  btn.addEventListener("click", () => {
-    box.classList.toggle("choices__box_open");
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (box.classList.contains("choices__box_open")) {
+      btn.classList.remove("choices__btn_open");
+      box.classList.remove("choices__box_open");
+    } else {
+      clearOpenClasses();
+      btn.classList.add("choices__btn_open");
+      box.classList.add("choices__box_open");
+    }
+
     adjustElementPosition(box);
+    window.addEventListener("resize", (e) => {
+      adjustElementPosition(box);
+    });
   });
 });
