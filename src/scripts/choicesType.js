2243123157df39dfdeead9455bac0788c4fa6a1a
StasGoryachev <goriachev07@mail.ru>
2024-05-21 17:53:45 +0300

    listType

--- src/scripts/choicesType.js
+++ src/scripts/choicesType.js
@@ -1,13 +1,17 @@
+import { ListType } from "./listType";
 import { store } from "./Store";
 
 export const initChoicesType = () => {
   const typeChoices = document.querySelector(".filter__choices_type");
+  const choicesBox = document.querySelector('.filter__choices-box_type')
 
   const updateTypeChoicesVisibility = () => {
-    const category = store.getCategories();
-
-    if (category.size) {
+    const categories = store.getCategories();
+    if (categories.size) {
       typeChoices.style.display = "";
+      choicesBox.textContent = '';
+      const listType = ListType([...categories]);
+      choicesBox.append(listType)
     } else {
       typeChoices.style.display = "none";
     }
---
+++ src/scripts/listType.jsx
@@ -0,0 +1,12 @@
+export const ListType = (categories) => (
+  <ul class="filter__type_list">
+    {categories.map((category) => (
+      <li class="filter__type_item">
+        <button class="filter__type_button" type="button">
+          {category}
+        </button>
+      </li>
+    ))}
+  </ul>
+);
+
