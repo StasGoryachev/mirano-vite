b113b367c600f431e2d8d68b8da4ad1858dda584
StasGoryachev <goriachev07@mail.ru>
2024-05-29 17:26:17 +0300

    ProductCard

--- src/scripts/ProductCard.jsx
+++ src/scripts/ProductCard.jsx
@@ -1,27 +1,34 @@
 import { API_URL } from "./APi";
 
-export const ProductCard = (product) => (
-  <li class="goods__item">
-    <article class="goods__card card">
-      <img
-        class="card__img"
-        src={`${API_URL}${product.photoUrl}`}
-        alt={product.name}
-      />
+export const ProductCard = (product) => {
+  return (
+    <li class="goods__item">
+      <article class="goods__card card">
+        <img
+          class="card__img"
+          src={`${API_URL}${product.photoUrl}`}
+          alt={product.name}
+        />
 
-      <div class="card__content">
-        <h3 class="card__title">{product.name}</h3>
+        <div class="card__content">
+          <h3 class="card__title">{product.name}</h3>
 
-        <div class="card__footer">
-          <p class="card__date-delivery">сегодня в&nbsp;14:00</p>
-          <button class="card__button">
-            <span class="card__price">
-              {product.price.toLocaleString()}&nbsp;₽
-            </span>
-            <span class="card__button-text">в&nbsp;корзину</span>
-          </button>
+          <div class="card__footer">
+            <p class="card__date-delivery">сегодня в&nbsp;14:00</p>
+            <button
+              class="card__button"
+              onMouseEnter={(e) => {
+                e.target.innerHTML = 'В корзину';
+              }}
+              onMouseLeave={(e) => {
+                e.target.innerHTML = `${product.price}&nbsp;Р`;
+              }}
+            >
+              {product.price}&nbsp;Р
+            </button>
+          </div>
         </div>
-      </div>
-    </article>
-  </li>
-);
+      </article>
+    </li>
+  );
+};
